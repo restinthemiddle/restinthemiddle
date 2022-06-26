a64114cf5b31e0518618bcdc14e0d96986fdc09a
Jens Schulze <info@jensschulze3000.de>
2022-06-27 00:43:41 +0200

    fix: use numeric only statuscode
    * Use numeric only HTTP statuscode
    * Cleanup zapwriter.go

--- pkg/zapwriter/zapwriter.go
+++ pkg/zapwriter/zapwriter.go
@@ -16,62 +16,7 @@ type Writer struct {
 	Logger *zap.Logger
 }
 
-func (w Writer) LogRequest(request *http.Request) (err error) {
-	query := ""
-	rawQuery := request.URL.RawQuery
-	if len(rawQuery) > 0 {
-		query = fmt.Sprintf("?%s", rawQuery)
-	}
-
-	headers := make([]string, 0)
-	for name, values := range request.Header {
-		for _, value := range values {
-			headers = append(headers, fmt.Sprintf("%s: %s", name, value))
-		}
-	}
-
-	bodyString := ""
-	if request.ContentLength > 0 {
-		bodyBytes, err := ioutil.ReadAll(request.Body)
-		if err != nil {
-			log.Fatal(err)
-			panic(err)
-		}
-
-		request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-
-		bodyString = string(bodyBytes)
-	}
-
-	w.Logger.Info("",
-		zap.String("request_method", request.Method),
-		zap.String("scheme", request.URL.Scheme),
-		zap.String("http_host", request.URL.Host),
-		zap.String("request", request.URL.Path),
-		zap.String("args", query),
-		zap.Strings("headers", headers),
-		zap.String("body", bodyString),
-	)
-
-	return nil
-
-	// requestRow := row{time.Now().Format(time.RFC3339Nano), request.Method, request.URL.Scheme, request.URL.Host, request.URL.Path, query, headers, bodyString}
-
-	// m, err := json.Marshal(requestRow)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// 	panic(err)
-	// }
-
-	// fmt.Println(string(m))
-
-	// return err
-}
-
 func (w Writer) LogResponse(response *http.Response) (err error) {
-	// request := response.Request
-	// w.LogRequest(request)
-
 	query := ""
 	rawQuery := response.Request.URL.RawQuery
 	if len(rawQuery) > 0 {
@@ -133,7 +78,7 @@ func (w Writer) LogResponse(response *http.Response) (err error) {
 		zap.String("args", query),
 		zap.Strings("request_headers", requestHeaders),
 		zap.String("post_body", requestBodyString),
-		zap.String("status", response.Status),
+		zap.Int("status", response.StatusCode),
 		zap.Time("connection_start", connectionStart),
 		zap.Time("connection_end", connectionEnd),
 		zap.Duration("connection_duration", connectionDuration),
